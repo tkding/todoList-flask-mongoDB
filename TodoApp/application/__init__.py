3b3bc6f1a8bae0b068c0c883e22c28a8c4dbeb69
tkding <tkding@ucsd.edu>
2023-10-02 00:02:44 -0400

    Update __init__.py

--- TodoApp/application/__init__.py
+++ TodoApp/application/__init__.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "9373e2050b7e48109500d1885696c3c985243c50"
-app.config["MONGO_URI"] = "mongodb+srv://tkding:Kern%400730@items.ibatret.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@items.ibatret.mongodb.net/?retryWrites=true&w=majority"
 
 #set up mongo db
 # mongodb_client = PyMongo(app)
